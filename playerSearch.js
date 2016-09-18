3206a2048dc44ca0508bd71a8e2eb862a95db72c
Jesse <jesse.chin@stonybrook.edu>
2016-09-17 21:46:14 -0400

    Create context menu only on install, not every time something is searched

--- playerSearch.js
+++ playerSearch.js
@@ -179,10 +179,12 @@ function searchMLB(info) {
   });
 };
 
-// Create the context menu
-chrome.contextMenus.create({
-  title: 'Search MLB Players: %s', 
-  contexts: ['selection'],
-  id: 'BaseballSearch'
+chrome.runtime.onInstalled.addListener(() => {
+  // Create the context menu
+  chrome.contextMenus.create({
+    title: 'Search MLB Players: %s',
+    contexts: ['selection'],
+    id: 'BaseballSearch'
+  });
+  chrome.contextMenus.onClicked.addListener(searchMLB);
 });
-chrome.contextMenus.onClicked.addListener(searchMLB);
